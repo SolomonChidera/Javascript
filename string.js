482a913c9d2be44129a11f9d3b116310832ba5d7
SolomonChidera <solochidera10@gmail.com>
2024-02-11 17:21:48 +0100

    Just practicing

---
+++ string.js
@@ -0,0 +1,6 @@
+let name = `solomon`;
+console.log(`My name is\n` + name);
+newline = "A newline is just a line in a new line \"\\n\""
+console.log(newline)
+half = `half of 100 is ${100 / 2}`;
+console.log(half)
